9224cd39893e3c0c39c266db854d443b06204b25
EricChukoltyansky <kazohelmet@gmail.com>
2021-11-23 18:15:40 +0200

    count even range

--- loop.js
+++ loop.js
@@ -1,3 +1,4 @@
+// 1
 // let printN = (n) => {
 //   for (let i = 0; i < n; i++) {
 //     console.log(i)
@@ -6,10 +7,65 @@
 
 // printN(10);
 
-let countX = (n,t) => {
-  if(n % t === 0) {
-      return n
-  }
-}
+// 2
 
-console.log(countX(10,5))
+// version 1
+// let countX = (n,t) => {
+//   for (let i = 0; i<n; i++) {
+//       if( i % t === 0) {
+//           console.log(i)
+//       }
+//   }
+// }
+
+// console.log(countX(10,5))
+
+// // version 2
+// let countX = (n,t) => {
+//     for(let i = 0; i<n; i+=t) {
+//         console.log(i)
+//     }
+// }
+
+// 3
+// version 1
+
+// let countEven = (n) => {
+//   let count = 0;
+//   n = n.toString().split("");
+//   for (let i = 0; i < n.length; i++) {
+//     if (n[i] % 2 === 0) {
+//       count++;
+//     }
+//   }
+//   return count;
+// };
+
+// console.log(countEven(1640));
+
+// version 2
+
+// function countDigits(n) {
+//   let total = 0;
+//   while (n) {
+//     n % 2 === 0 ? total++ : total;
+//     n = Math.floor(val / 10);
+//   }
+//   return total;
+// }
+
+// 4
+
+// version 1
+let countEvenOnRange = (n) => {
+  
+  let count = 0;
+  for( let i = 111; i<n ; i++) {
+     console.log(i)
+     }
+     return count;
+  }
+  
+
+
+console.log(countEvenOnRange(150))

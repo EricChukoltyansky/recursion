c1259e1bb06e6a7e6f9beb8670620c1bf2d565c1
EricChukoltyansky <kazohelmet@gmail.com>
2021-11-23 14:58:39 +0200

    print

---
+++ loop.js
@@ -0,0 +1,15 @@
+// let printN = (n) => {
+//   for (let i = 0; i < n; i++) {
+//     console.log(i)
+//   }
+// };
+
+// printN(10);
+
+let countX = (n,t) => {
+  if(n % t === 0) {
+      return n
+  }
+}
+
+countX(10,5)

bbd23d920eb172407293299936df155db136f674
EricChukoltyansky <kazohelmet@gmail.com>
2021-11-23 21:07:02 +0200

    email

--- loop.js
+++ loop.js
@@ -57,27 +57,85 @@
 // 4
 
 // version 1
-let countEvenOnRange = (n) => {
-  let count = 0;
-  
+// let countEvenOnRange = (n) => {
+//   let count = 0;
 
-  for (let i = 112; i <= n; i++) {
-       if (i % 10 % 2 === 0 ) {
-           count++
-       }
-     if(i/10%2 === 0){
-         count++
-     }
-     if(i/10/10%2 === 0) {
-         count++
-     }
-      
-       console.log(Math.floor(i/10));
-    
-    
+//   for (let i = 112; i <= n; i++) {
+//     if ((i % 10) % 2 === 0) {
+//       count++;
+//     }
+//     if ((i / 10) % 2 === 0) {
+//       count++;
+//     }
+//     if ((i / 10 / 10) % 2 === 0) {
+//       count++;
+//     }
+
+//     console.log(Math.floor(i / 10));
+//   }
+//   return count;
+// };
+
+// console.log(countEvenOnRange(120));
+
+// 5
+// let printReverse = () => {
+//     for( let i = 100; i>=0; i-=2) {
+//         console.log(i)
+//     }
+// }
+
+// printReverse()
+
+// 6
+// let quad = (a,b,c) => {
+//     {
+//         let result = (-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+//         let result2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+//         return result + "<br>" + result2;
+//     }
+// }
+
+// console.log(quad(3,8,2))
+
+// 7
+// countABC = (str) => {
+//   let counts = {};
+
+//   let char, count;
+
+//   for (let i = 0; index < str.length; i++) {
+//     char = str.charAt(i);
+
+//     count = counts[char];
+
+//     counts[char] = count ? count + 1 : 1;
+//   }
+// };
+
+// 8
+// function capitalize(word) {
+//   let capitalizedWord="", char=0;
+//   for (let i = 0; i < word.length; i++) {
+//     let firstChar = word.charCodeAt(i);
+//     if (firstChar >= 97 && firstChar <= 122) {
+//       char = String.fromCharCode(firstChar - 32);
+
+//     //   console.log(char);
+//     }
+//     capitalizedWord += char;
+//   }
+//   return capitalizedWord;
+// }
+
+// console.log(capitalize("mother"));
+
+// 9 
+
+function validateEmail(email) 
+    {
+        let re = /\S+@\S+\.\S+/;
+        return re.test(email);
     }
-    return count;
-  }
 
 
-console.log(countEvenOnRange(120));
